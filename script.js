9d8f3d652ccc7199367c1b9ea54cbfe3dd25f93a
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-09-24 00:19:00 +0500

    created playRound()
    created playRound() function to play a round of the game

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
+//get computer's choice
 let getComputerChoice = () => {
-    let choices = ["rock", 'paper', 'scissors'];
-    return choices[Math.floor(Math.random() * 3)];
-}
+  let choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)];
+};
 
-let userChoice = () => {
-    return (prompt("Enter your choice (rock/paper/scissors):").toLowerCase());
-}
+//get user's choice
+let getuserChoice = () => {
+  let choiceMade = prompt(
+    "Enter your choice (rock/paper/scissors):"
+  ).toLowerCase();
+  return choiceMade;
+};
+
+//play a round
+let playRound = (param1, param2) => {
+  let [computerSelection, userSelection] = [
+    getComputerChoice(),
+    getuserChoice(),
+  ];
+
+  let result = "";
+
+  if (computerSelection === userSelection) {
+    console.log("it's a tie!");
+    result = "tie";
+  } else if (
+    (computerSelection === "rock" && userSelection === "paper") ||
+    (computerSelection === "paper" && userSelection === "scissors") ||
+    (computerSelection === "scissors" && userSelection === "rock")
+  ) {
+    console.log(`You Win! ${userSelection} beats ${computerSelection}`);
+    result = "user";
+  } else {
+    console.log(`You Lose! ${computerSelection} beats ${userSelection}`);
+    result = "computer";
+  }
+};
+
+playRound();
